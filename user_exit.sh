3e21418865b92c94d6baaff578fd7deef9ded90f
Daniel Parkes <dparkes@redhat.com>
2024-11-08 16:15:43 +0100

    Update user_exit.sh

--- user_exit.sh
+++ user_exit.sh
@@ -9,8 +9,8 @@ cp ./deploy_cluster.sh /root/scripts
 cp ./break_and_fix1.yaml /root/scripts
 chown $USER $USER_HOME/cli-helper-1527*
 chmod 644 $USER_HOME/cli-helper-1527*
-ssh ceph-node01 "mkdir /root/scripts"
-scp ./purge_cluster.sh root@ceph-node01:/root/scripts
+ssh ceph-node1 "mkdir /root/scripts"
+scp ./purge_cluster.sh root@ceph-node1:/root/scripts
 
 ## Configure html cli helper file as the default Firefox home page
 PROFILE_DIR=$(find $USER_HOME/.mozilla/firefox -type d -name "*.default*" | head -n 1)
